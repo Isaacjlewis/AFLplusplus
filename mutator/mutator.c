8bb1b1b2b676db80547daec038786299b80f2fe4
Isaac Lewis <jiajiaisaac@gmail.com>
2023-06-15 16:44:45 +0200

    Update mutator.c

--- mutator/mutator.c
+++ mutator/mutator.c
@@ -18,7 +18,6 @@ unsigned int afl_custom_fuzz_count(void *data, const unsigned char *buf, size_t 
 }
 
 size_t afl_custom_fuzz(void* udata, unsigned char *buf, size_t buf_size, unsigned char **out_buf, unsigned char *add_buf, size_t add_buf_size, size_t max_size){
-  sleep(2);
   return 0;
 }
 
